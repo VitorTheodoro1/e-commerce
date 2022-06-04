5b06e8b07c73d13d5c1c1923ea2c77d0e1d7a683
Vitor Theodoro <vitor-theodoro@hotmail.com>
2022-06-04 13:36:59 -0300

    add: checkout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import { Routes, Route } from "react-router-dom";
 import Shop from "./routes/shop/shop.component";
+import CheckOut from "./routes/checkout/checkout.component";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<Home></Home>}></Route>
         <Route path="auth" element={<Authentication></Authentication>}></Route>
         <Route path="shop" element={<Shop></Shop>}></Route>
+        <Route path="checkout" element={<CheckOut></CheckOut>}></Route>
       </Route>
     </Routes>
   );
